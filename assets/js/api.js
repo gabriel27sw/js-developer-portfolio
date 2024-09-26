6b43dd14a7e18a5ed4c5850fdcf6f45420729ecb
gabriel27sw <gabriel27sw@gmail.com>
2024-09-26 00:39:36 -0300

    Portifolio API

---
+++ assets/js/api.js
@@ -0,0 +1,6 @@
+async function fetchProfileData() {
+    const url = `https://raw.githubusercontent.com/gabriel27sw/js-developer-portfolio/refs/heads/projeto-base/data/profile.json`
+    const fetching = await fetch(url)
+    return await fetching.json()
+}
+
